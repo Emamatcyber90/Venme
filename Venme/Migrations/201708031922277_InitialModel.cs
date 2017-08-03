edb68912ee7657b72646a6c9f8ef06d7bac34167
zhanwenchen <phil.zhanwen.chen@gmail.com>
2017-08-03 19:24:53 +0000

    Added Initial Migration by Add-Migration InitialModel -IgnoreChanges

---
+++ Venme/Migrations/201708031922277_InitialModel.cs
@@ -0,0 +1,16 @@
+namespace Venme.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
